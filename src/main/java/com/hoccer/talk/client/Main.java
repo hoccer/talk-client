109b3e6f766b3aac9bc2ec07ae080c4252160e2d
Ingo Albrecht <prom@berlin.ccc.de>
2013-02-22 08:58:08 +0100

    Various progress. Completed first connection handling and RPC.

--- src/main/java/com/hoccer/talk/client/Main.java
+++ src/main/java/com/hoccer/talk/client/Main.java
@@ -1,25 +1,65 @@
 package com.hoccer.talk.client;
 
 import java.net.URI;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
-import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocketClient;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.websocket.WebSocketClientFactory;
 
+import better.jsonrpc.client.JsonRpcClient;
+import better.jsonrpc.server.JsonRpcServer;
+import better.jsonrpc.util.ProxyUtil;
+import better.jsonrpc.websocket.JsonRpcWsClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoccer.talk.model.TalkDelivery;
+import com.hoccer.talk.model.TalkMessage;
+import com.hoccer.talk.rpc.TalkRpcClient;
+import com.hoccer.talk.rpc.TalkRpcServer;
+
 public class Main {
+	
+	private static final Logger log = Logger.getLogger(Main.class);
 
 	public static void main(String[] args) {
 		try {
 			WebSocketClientFactory f = new WebSocketClientFactory();
 			f.start();
 			
-			for(int i = 0; i < 1000; i++) {
-				Socket socket = new Socket();
-				WebSocketClient c = f.newWebSocketClient();
-				c.open(new URI("ws://localhost:8080/"), socket);
+			JsonRpcWsClient connection = new JsonRpcWsClient(
+					f, new ObjectMapper(), new URI("ws://localhost:8080/"));
+			JsonRpcServer srv = new JsonRpcServer(TalkRpcClient.class);
+			connection.setHandler(new TalkRpcClient() {
+				@Override
+				public void outgoingDelivery(TalkDelivery d) {
+					log.info("outgoing delivery");
+				}
+				@Override
+				public void incomingDelivery(TalkDelivery d, TalkMessage m) {
+					log.info("incoming delivery");
+				}
+			});
+			connection.setServer(srv);
+			connection.connect();
+			
+			TalkRpcServer server = (TalkRpcServer)	
+					ProxyUtil.createClientProxy(
+							Main.class.getClassLoader(),
+							TalkRpcServer.class, connection);
+			
+			Thread.sleep(1000);
+			
+			for(int i = 0; i < 10; i++) {
+				server.deliveryConfirm(new TalkDelivery());
+				Thread.sleep(1000);
 			}
+			
+			Thread.sleep(500);
+			
+			connection.disconnect();
+			
+			Thread.sleep(500);
+			
+			System.exit(0);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
