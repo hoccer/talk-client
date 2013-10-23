2c2f3382c77dd59282cf7cf1370a78dba83a803e
Ingo Albrecht <prom@berlin.ccc.de>
2013-10-23 22:56:19 +0200

    android: SSL should now use the newest TLS available and AES only

--- src/main/java/com/hoccer/talk/client/XoClientConfiguration.java
+++ src/main/java/com/hoccer/talk/client/XoClientConfiguration.java
@@ -36,6 +36,13 @@ public class XoClientConfiguration {
     /** Variable reconnect backoff delay - time maximum (seconds) */
     public static final double RECONNECT_BACKOFF_VARIABLE_MAXIMUM = 120.0;
 
+    /** Enable the session cache to improve connection latency */
+    public static final boolean TLS_SESSION_CACHE_ENABLED = true;
+
+    /** Limit the session cache to a small size */
+    public static final int TLS_SESSION_CACHE_SIZE = 3;
+
+    /** Allow only AES-based cipher suites */
     public static final String TLS_CIPHERS[] = {
         "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
         "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
@@ -44,15 +51,11 @@ public class XoClientConfiguration {
         "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
         "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
         "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
-        "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
-        "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
         "TLS_RSA_WITH_AES_128_CBC_SHA",
         "TLS_RSA_WITH_AES_256_CBC_SHA",
-        "SSL_RSA_WITH_3DES_EDE_CBC_SHA",
-        "SSL_RSA_WITH_RC4_128_SHA",
-        "SSL_RSA_WITH_RC4_128_MD5",
     };
 
+    /** Allow only TLS 1.1 and up */
     public static final String TLS_PROTOCOLS[] = {
             "TLSv1.2",
             "TLSv1.1",
