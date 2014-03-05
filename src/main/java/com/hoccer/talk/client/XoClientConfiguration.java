c9710b54226b3e21d6f5c87679c0aea3c408a5c5
Pavel Mayer <pavel@hoccer.com>
2014-03-05 16:52:26 +0100

    - reordered and added SSL cipher modes

--- src/main/java/com/hoccer/talk/client/XoClientConfiguration.java
+++ src/main/java/com/hoccer/talk/client/XoClientConfiguration.java
@@ -47,15 +47,17 @@ public class XoClientConfiguration {
 
     /** Allow only AES-based cipher suites */
     public static final String TLS_CIPHERS[] = {
-        "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+        "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+        "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
         "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
-        "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
         "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
-        "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+        "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+        "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
         "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+        "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
         "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
         "TLS_RSA_WITH_AES_128_CBC_SHA",
-        "TLS_RSA_WITH_AES_256_CBC_SHA",
+        "TLS_RSA_WITH_AES_256_CBC_SHA"
     };
 
     /** Allow only TLS 1.1 and up */
