97bdabf6274ec046e35c6d5aba3d1c7d49e06806
Julian Krumow <julian.krumow@artcom.de>
2014-05-02 11:39:58 +0200

    - cleaned up imports. - added interactive message - added javadoc

--- src/main/java/com/hoccer/talk/client/IXoAlertListener.java
+++ src/main/java/com/hoccer/talk/client/IXoAlertListener.java
@@ -1,9 +1,25 @@
 package com.hoccer.talk.client;
 
 
+/**
+ * This interface is used by the client to handle alert messages which have been generated
+ * internally or received from the server.
+ */
 public interface IXoAlertListener {
 
+    /**
+     * Client issued an internal alert message.
+     *
+     * @param title   The title of the alert message
+     * @param message The text of the alert message
+     */
     public void onInternalAlert(String title, String message);
+
+    /**
+     * Client received an alert message from the server.
+     *
+     * @param message The alert message as string
+     */
     public void onAlertMessageReceived(String message);
 
 }
